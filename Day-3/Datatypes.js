380ef25d466d23b55234f8a8dc763b9f0c41c43d
Adarsh pratap <schhotu092@gmail.com>
2025-06-13 20:02:52 +0530

    Datatypes

---
+++ Day-3/Datatypes.js
@@ -0,0 +1,23 @@
+// Number
+let length = 16;
+let weight = 7.5;
+
+// Strings
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let X = true;
+let Y = false;
+
+// Object
+const Person = {
+    FirstName : "John",
+    LastName : "Doe"
+}
+
+//Array Object
+const cars = ["saab","volvo","Bmw"];
+
+// Date Object
+const Date = new Date("2025-06-13");
