bb1fb4ee9cb539a0776bebcad527e8a0cd8cee53
Adarsh pratap <schhotu092@gmail.com>
2025-06-17 11:45:40 +0530

    object.entries

---
+++ Day-7/object.entries.js
@@ -0,0 +1,7 @@
+const fruits = {Banana:300,Oranges:200,Apples:500};
+let text = "";
+for(let[fruit,value] of Object.entries(fruits)){
+    text += fruit + ":" + value +"<br>";
+}
+
+console.log(text);
